1751d5294e5529dfc2d567bc6c1106e57ce595f3
Grupo RETEC <artmosfera@gruporetec.com.br>
2025-02-25 18:19:27 -0300

    chore: delete prodoucts page

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -26,9 +26,6 @@ export default function Navbar({ activeTab }: { activeTab: string; }) {
               <Link className={isTabActive('home')} href="/">Home</Link>
             </nav>
             <nav className={styles.navItem}>
-              <Link className={isTabActive('products')} href="/produtos">Produtos</Link>
-            </nav>
-            <nav className={styles.navItem}>
               <Link className={isTabActive('projects')} href="/obras">Obras</Link>
             </nav>
             <nav className={styles.navItem}>
