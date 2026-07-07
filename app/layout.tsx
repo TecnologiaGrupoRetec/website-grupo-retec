50cda689b1e983b7036c882350f860b1007ec45a
Pedro - Tecnologia Grupo Retec <tecnologia@gruporetec.com.br>
2026-07-07 10:53:59 -0300

    Adicionando o script de rastreamento das páginas do site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.scss";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Grupo RETEC",
@@ -16,7 +17,24 @@ export default function RootLayout({
       <body>
         {children}
       </body>
-      <script async type="text/javascript" src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></script>
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18266697931"
+        strategy="afterInteractive"
+      />
+      <Script id="google-tag" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-18266697931');
+        `}
+      </Script>
+      {/* RD Station Forms */}
+      <Script
+        src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"
+        strategy="afterInteractive"
+      />
     </html>
   );
 }
