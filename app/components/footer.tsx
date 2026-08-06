07172cc1e4fe7e406541299c8153d07a62014b1a
Pedro - Tecnologia Grupo Retec <tecnologia@gruporetec.com.br>
2026-08-06 10:40:08 -0300

    Mudando 'servicos' por 'portfolio'

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,7 +4,7 @@ import styles from "./styles/footer.module.scss";
 
 const footerGroups = [
   {
-    title: "Serviços",
+    title: "Portfólio",
     links: [
       { label: "Produtos", href: "/solucoes" },
       { label: "Obras Fornecidas", href: "/obras" },
